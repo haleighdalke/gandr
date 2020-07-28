c5914ac0cf163aa3b73d1ae9b7163375765c0674
haleighdalke <haleighdalke@gmail.com>
2020-07-28 19:34:57 -0400

    fixed cors.rb

--- gandr-backend/db/seeds.rb
+++ gandr-backend/db/seeds.rb
@@ -1,16 +1,15 @@
 require './app/adapter/api_link.rb'
 
 # Artwork Seed Data
-# ApiLink.get_data("sunflowers")
+ApiLink.get_data("sunflowers")
 
 # User Seed Data
 User.create(username: "haleigh_d")
 User.create(username: "iona_b")
 
 # Like Seed Data
-Like.create(user_id: User.sample.id, artwork_id: Artwork.sample.id)
+Like.create(user_id: User.all.sample.id, artwork_id: Artwork.all.sample.id)
 
 # Comment Seed Data
 
 puts "Seeds done!"
-
