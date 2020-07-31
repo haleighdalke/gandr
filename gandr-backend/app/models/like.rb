f22bc35f86823a5bc9d6020019e0afcc29506ef3
iona-b <ionabrabender@gmail.com>
2020-07-30 17:39:56 -0700

    merge

--- gandr-backend/app/models/like.rb
+++ gandr-backend/app/models/like.rb
@@ -2,5 +2,13 @@ class Like < ApplicationRecord
 
     belongs_to :artwork
     belongs_to :user
+
+    def self.dedupe
+        grouped = all.group_by{|like| [like.artwork_id, like.user_id] }
+        grouped.values.each do |duplicates|
+          first_one = duplicates.shift
+          duplicates.each{|double| double.destroy}
+        end
+    end
     
 end
